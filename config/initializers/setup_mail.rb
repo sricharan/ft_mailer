37134edd808fdedce4cbdd3295bae48e558bfde7
Sricharan Sunder <sricharan92@gmail.com>
2011-10-31 15:01:55 +0530

    try 1 gmail mail

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'gmail.com',
+  :user_name            => 'sricharan92@gmail.com',
+  :password             => 'surya54sri7e',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+
