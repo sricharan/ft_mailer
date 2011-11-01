3be33e2976f208fd82aac14a2bc4b744afb944ea
Sricharan Sunder <sricharan92@gmail.com>
2011-11-01 13:35:42 +0530

    kjb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "10.3.100.244",
-  :port                 => 587,
-  :domain               => '10.3.100.244',
+  :port                 => "587",
+  :domain               => 'iitkgp.ac.in',
   :user_name            => 'sricharan@iitkgp.ac.in',
   :password             => 'sricharan09MT3008',
   :authentication       => 'plain',
